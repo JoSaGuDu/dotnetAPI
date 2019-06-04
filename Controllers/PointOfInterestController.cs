e4ef0acdc4432f6c2fc6246f481fef6cbfe17827
Jose S <reaktechnologiesdsignws@gmail.com>
2019-06-04 17:29:05 -0400

    Partially update action added --PATCH action

--- Controllers/PointOfInterestController.cs
+++ Controllers/PointOfInterestController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Web.Http;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace dotnet_asp_API.Controllers
 {
@@ -16,8 +17,6 @@ namespace dotnet_asp_API.Controllers
         //I need to log in the console
         //So I demand an instance of the required service factory
         private ILogger<PointsOfInterestController> _logger;//ILogger(I for Interface) instance provide by the container usin the T  technique: use the type of the category name: nameSpace<categoryName> _nameOfInstance
-
-        //Now, I  
 
         //Routing template. Routes must reflect parent child relationships of data.
         [HttpGet("{cityId}/pointsOfInterest")]//if parameter here, add it to the acction function
@@ -141,6 +140,68 @@ namespace dotnet_asp_API.Controllers
             existentPointOfInterest.Name = pointOfInterestUpdate.Name;
             existentPointOfInterest.Description = pointOfInterestUpdate.Description;
 
+            return NoContent();
+        }
+
+        //Partial resource update (PATCH). PATCH uses A class that allow us to deserialize a list of operations to be performed on a Json document (Json Patch RFC 6905). The lists is in Json format.The document will be sended by the consumer via body in the PATCH action and will contain the list of task to perform (replace, copy...)  and the id of the item to be affected and the key: value pairs to be affected on the item. HttpPatchAttribute request body example: [{"op": "replace", "path": "propertyName", "value": "propertyValue"}, {"op": "operation2", "path": "property2Name", "value": "property2Value"}...].
+        [HttpPatch("{cityId}/pointsOfInterest/{id}")]
+        //Action:Partial update of a point of interest of a city
+        public IActionResult PartialUpdatePointOfInterest(int cityId, int pointOfinterestToUpdateId, [FromBody]  JsonPatchDocument<PointsOfInterestDtoResourcePartialUpdate> patchDoc)
+        {
+            //Body is not empty
+            if (patchDoc == null)
+            {
+                return new BadRequestObjectResult("Invalid informations");
+            }
+
+            //Find the city of the point to be updated
+            var cityOfPoints = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+            if (cityOfPoints == null)
+            {
+                return new NotFoundResult();
+            }
+
+            //Find the the point to be updated
+            //find the point of interest 
+            var existentPointOfInterest = cityOfPoints.PointsOfInterest.FirstOrDefault(point => point.Id == pointOfinterestToUpdateId);
+            if (existentPointOfInterest == null)
+            {
+                return new NotFoundResult();
+            }
+
+            //Map <PointOfInterestDto>existentPointOfInterest to <PointOfInterestDtoResourcePartialUpdate>pointOfInterestToPartiallyUpdate
+            var pointOfinterestToPartiallyUpdate =
+                new PointsOfInterestDtoResourcePartialUpdate()
+                {
+                    Name = existentPointOfInterest.Name,
+                    Description = existentPointOfInterest.Description
+                };
+
+            //Apply operations listed on patchDoc to item to be partially updated and the MOdelState to validate the input
+            patchDoc.ApplyTo(pointOfinterestToPartiallyUpdate, ModelState);
+
+            //validate input
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);//returns the ModelState that will display errors when user inpunt doesn't comply data anottations
+            }
+
+            if (pointOfinterestToPartiallyUpdate.Description == pointOfinterestToPartiallyUpdate.Name)
+            {
+                ModelState.AddModelError("CustomErrorKey", "CustomError message: The provided description should be different from the name");
+                return new BadRequestObjectResult(ModelState);//returns the ModelState thar will display errors when user inpunt doesn't comply data anottations
+            }
+
+            TryValidateModel(pointOfinterestToPartiallyUpdate);
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);//returns the ModelState that will display errors when user inpunt doesn't comply data anottations
+            }
+
+
+            //Partially update the item //Fully update the point
+            existentPointOfInterest.Name = pointOfinterestToPartiallyUpdate.Name;
+            existentPointOfInterest.Description = pointOfinterestToPartiallyUpdate.Description;
             return NoContent();
         }
     }
---
+++ Models/PointsOfInterestDto.ResourcePartialUpdate.cs
@@ -0,0 +1,15 @@
+//This classes are also DTOs but are specialized to transport data in the way the database will undestand. 
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_asp_API
+{
+    public class PointsOfInterestDtoResourcePartialUpdate
+    {
+        [Required(ErrorMessage = "You should provide a name value. ")]//Using data annotations that helps to validate user input. Those rules have to be chequed with help of the ModelState in the controller.
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
